9f9c42ff072b13691d2ddf00ff60b2d7c196668e
Anni3607 <anirudhapujari7@gmail.com>
2026-03-16 20:43:17 +0530

    Update forecasting.py

--- modules/forecasting.py
+++ modules/forecasting.py
@@ -1,23 +1,19 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
-
 def forecast_cost(df):
 
-    # Create index for regression
-    X = np.arange(len(df)).reshape(-1, 1)
+    df = df.copy()
+    df["t"] = np.arange(len(df))
 
-    # Target variable
-    y = df["cost"].values
+    X = df[["t"]]
+    y = df["cost"]
 
-    # Train model
     model = LinearRegression()
     model.fit(X, y)
 
-    # Predict next time step
-    future_index = np.array([[len(df)]])
+    next_t = np.array([[len(df)]])
 
-    prediction = model.predict(future_index)
+    prediction = model.predict(next_t)
 
-    # prediction is an array like [2.84]
     return float(prediction[0])
